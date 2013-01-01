2ac8a1369f2bf9f4090b2f6e22b0aacfd916a161
Igor Bondarenko <jetmind2@gmail.com>
2013-01-01 21:12:04 +0200

    ticket:1 add basic tests

--- jetmind-test/src/test/java/com/cc/MainActivityTest.java
+++ jetmind-test/src/test/java/com/cc/MainActivityTest.java
@@ -1,19 +1,56 @@
 package com.cc;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.text.IsEqualIgnoringWhiteSpace.equalToIgnoringWhiteSpace;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.xtremelabs.robolectric.RobolectricTestRunner;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
-	
-	@Test
-	public void testShowsHello() throws Exception {
-		String hello = new MainActivity().getResources().getString(R.string.hello_world);
-		assertThat(hello, equalTo("Hello world!"));
-	}
+
+    private MainActivity activity;
+    private TextView textFio;
+    private TextView textBirth;
+    private TextView textContacts;
+    private TextView textBio;
+    private ImageView imagePhoto;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = new MainActivity();
+        activity.onCreate(null);
+        textFio = (TextView) activity.findViewById(R.id.textFio);
+        textBirth = (TextView) activity.findViewById(R.id.textBirth);
+        textContacts = (TextView) activity.findViewById(R.id.textContacts);
+        textBio = (TextView) activity.findViewById(R.id.textBio);
+        imagePhoto = (ImageView) activity.findViewById(R.id.imagePhoto);
+    }
+
+    @Test
+    public void widgetsAreVisible() {
+        assertThat(textFio.getVisibility(), equalTo(View.VISIBLE));
+        assertThat(textBirth.getVisibility(), equalTo(View.VISIBLE));
+        assertThat(textContacts.getVisibility(), equalTo(View.VISIBLE));
+        assertThat(textBio.getVisibility(), equalTo(View.VISIBLE));
+        assertThat(imagePhoto.getVisibility(), equalTo(View.VISIBLE));
+    }
+
+    @Test
+    public void shouldShowUserInfo() {
+        assertThat(textFio.getText().toString(), equalTo("Igor Bondarenko"));
+        assertThat(textBirth.getText().toString(), equalTo("March 29, 2012"));
+        String expected = "Email: jetmind2@gmail.com\nSkype: jetmind";
+        assertThat(textContacts.getText().toString(), equalToIgnoringWhiteSpace(expected));
+        expected = "Working as a Python developer at 42 Coffee Cups";
+        assertThat(textBio.getText().toString(), equalToIgnoringWhiteSpace(expected));
+    }
 }
