932504a5e0c021a9e01f56259ea870f254999a0c
Igor Bondarenko <jetmind2@gmail.com>
2013-01-16 15:08:55 +0200

    ticket:2 basic facebook login (with login button)

--- jetmind-test/src/main/java/com/cc/MainActivity.java
+++ jetmind-test/src/main/java/com/cc/MainActivity.java
@@ -1,53 +1,51 @@
 package com.cc;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     private static String TAG = "jetmind-test";
-
     private static long USER_ID = 1;
+    private LoginFragment loginFragment;
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.activity_main);
-
-        DatabaseHandler db = new DatabaseHandler(this);
-        User user = db.getUser(USER_ID);
-
-        if (user != null) {
-            TextView textFio = (TextView) findViewById(R.id.textFio);
-            textFio.setText(user.getName() + " " + user.getSurname());
-            TextView textBirth = (TextView) findViewById(R.id.textBirth);
-            textBirth.setText(
-                    new SimpleDateFormat(
-                            "MMMM dd, yyyy", Locale.getDefault()).format(user.getBirth()));
-            TextView textBio = (TextView) findViewById(R.id.textBio);
-            textBio.setText(user.getBio());
-            StringBuilder sb = new StringBuilder();
-            if (user.getContacts() != null) {
-                for (Contact c : user.getContacts()) {
-                    sb.append(c.getLabel() + ": " + c.getValue() + "\n");
-                }
-            }
-            sb.deleteCharAt(sb.length() - 1);
-            TextView textContacts = (TextView) findViewById(R.id.textContacts);
-            textContacts.setText(sb.toString());
+        //        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager()
+            .beginTransaction()
+            .add(android.R.id.content, loginFragment)
+            .commit();
+        } else {
+            loginFragment = (LoginFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
         }
+
+        //        DatabaseHandler db = new DatabaseHandler(this);
+        //        User user = db.getUser(USER_ID);
+        //
+        //        if (user != null) {
+        //            TextView textFio = (TextView) findViewById(R.id.textFio);
+        //            textFio.setText(user.getName() + " " + user.getSurname());
+        //            TextView textBirth = (TextView) findViewById(R.id.textBirth);
+        //            textBirth.setText(
+        //                    new SimpleDateFormat(
+        //                            "MMMM dd, yyyy", Locale.getDefault()).format(user.getBirth()));
+        //            TextView textBio = (TextView) findViewById(R.id.textBio);
+        //            textBio.setText(user.getBio());
+        //            StringBuilder sb = new StringBuilder();
+        //            if (user.getContacts() != null) {
+        //                for (Contact c : user.getContacts()) {
+        //                    sb.append(c.getLabel() + ": " + c.getValue() + "\n");
+        //                }
+        //            }
+        //            sb.deleteCharAt(sb.length() - 1);
+        //            TextView textContacts = (TextView) findViewById(R.id.textContacts);
+        //            textContacts.setText(sb.toString());
+        //        }
     }
 
 }
